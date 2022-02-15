d1821995943d1d57cafd4d6e93e349774ef6fff9
Jean Charbonneau <jcharbonneau.ext@simplonformations.onmicrosoft.com>
2022-02-15 09:10:00 +0100

    Scrapper as a function

---
+++ scrapper.py
@@ -0,0 +1,33 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+def getTextFromUrl(url):
+    res = requests.get(url)
+    html_page = res.content
+    soup = BeautifulSoup(html_page, 'html.parser')
+
+    output = ''
+
+    for string in soup.stripped_strings:
+        output += ' ' + string.lower()
+
+    output = re.sub("[^a-zA-Zàâäéèêëïîôöùûüÿç]+", " ", output)
+
+    smallerWordsRemoved = ''
+
+    for word in output.split(' '):
+        if(len(word) > 2):
+            smallerWordsRemoved += ' ' + word
+
+    return smallerWordsRemoved
+
+print(getTextFromUrl('https://fr.wikipedia.org/wiki/R%C3%A9cepteur_sensoriel'))
+
+# link = soup.find('div', {'id': 'mw-content-text'}).find_all(('p', 'li'))
+
+# output = ''
+
+# for i in link:
+#     output += i.text
+
