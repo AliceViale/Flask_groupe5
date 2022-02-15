6f2558726b1eb3bcc61768ac584e479e01bb25fd
Victor Freyer <victor.freyer@gmail.com>
2022-02-15 11:05:05 +0100

    c'est bien rangé et Jean il a bien travaillé

--- application/forestry.py
+++ application/forestry.py
@@ -1,6 +1,20 @@
 from networkx import Graph
-from scrapper import get_json_tree
 
-url = 'https://fr.wikipedia.org/wiki/Lucifer'
-json_tree_dict = dict(get_json_tree(url))
-tree = Graph(json_tree_dict)
+def dict_word_occurence_to_tree(dict_tree):
+    tree=Graph()
+    all_tuple = list(dict_tree.items())
+    fifo = []
+    fifo.append(all_tuple[1])
+    fifo.append(all_tuple[2])
+    tree.add_node(all_tuple[0])
+    tree.add_node(all_tuple[1])
+    tree.add_node(all_tuple[2])
+    tree.add_edge(all_tuple[0],all_tuple[1])
+    tree.add_edge(all_tuple[0],all_tuple[2])
+    for t in range(3,len(all_tuple)):
+        fifo.append(all_tuple[t])
+        if len(list(tree.neighbors(fifo[0]))) >= 3:
+            trash = fifo.pop(0)
+        tree.add_node(all_tuple[t])
+        tree.add_edge(fifo[0],all_tuple[t])
+    return tree
--- application/scrapper.py
+++ application/scrapper.py
@@ -2,12 +2,23 @@ import requests, operator, json
 from bs4 import BeautifulSoup
 from application import db
 from .models import Knowledge
+import re
+
+# def page_reading(url):
+#     response = requests.get(url)
+#     soup = BeautifulSoup(response.content,features="html.parser")
+#     link = soup.stripped_strings
+#     return link
 
 def page_reading(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content,features="html.parser")
-    link = soup.find('div', {'class': 'mw-parser-output'}).find_all('p')
-    return link
+    res = requests.get(url)
+    html_page = res.content
+    soup = BeautifulSoup(html_page, 'html.parser')
+    allWords = ''
+    for strPiece in soup.stripped_strings:
+        allWords += ' ' + strPiece.lower()
+    allWords = re.sub("[^a-z0-9A-Zàâäéèêëïîôöùûüÿç]+", " ", allWords)
+    return allWords
 
 def word_counting(text):
     counts = dict()
@@ -19,25 +30,31 @@ def word_counting(text):
             counts[word] = 1
     return counts
 
-def word_count_merging(dict1, dict2):
-    merged = dict1.copy()
-    for key,value in dict2.items():
-        if key in merged.keys():
-            merged[key] += value
-        else:
-            merged[key] = value
-    return merged
+# def word_count_merging(dict1, dict2):
+#     merged = dict1.copy()
+#     for key,value in dict2.items():
+#         if key in merged.keys():
+#             merged[key] += value
+#         else:
+#             merged[key] = value
+#     return merged
 
-def full_page_word_counts(url):
-    page = page_reading(url)
-    final_count = dict()
-    for t in page:
-        word_count = word_counting(t.text)
-        final_count = word_count_merging(final_count,word_count)
-    return final_count
+# def full_page_word_counts(url):
+#     page = page_reading(url)
+#     final_count = dict()
+#     for t in page:
+#         word_count = word_counting(t)
+#         final_count = word_count_merging(final_count,word_count)
+#     return final_count
+
+# def get_json_tree(url):
+#     result = full_page_word_counts(url)
+#     sorted_r = dict(sorted(result.items(), key=operator.itemgetter(1),reverse=True))
+#     json_result = json.dumps(sorted_r,ensure_ascii=False)
+#     return json_result
 
 def get_json_tree(url):
-    result = full_page_word_counts(url)
+    result = word_counting(page_reading(url))
     sorted_r = dict(sorted(result.items(), key=operator.itemgetter(1),reverse=True))
     json_result = json.dumps(sorted_r,ensure_ascii=False)
     return json_result
@@ -49,3 +66,18 @@ def put_into_db(url,json_tree):
         db.session.commit()
     else:
         print("Nice try !")
+
+# def getTextFromUrl(url : str): #, nbChars : int):
+#     res = requests.get(url)
+#     html_page = res.content
+#     soup = BeautifulSoup(html_page, 'html.parser')
+#     allWords = ''
+#     for strPiece in soup.stripped_strings:
+#         allWords += ' ' + strPiece.lower()
+#     allWords = re.sub("[^a-z0-9A-Zàâäéèêëïîôöùûüÿç]+", " ", allWords)
+#     # allWords = re.sub(r"""[!?".<>(){}@%&*/[/]""", "", allWords)
+#     # smallerWordsRemoved = ''
+#     # for word in allWords.split(' '):
+#     #     if(len(word) > (nbChars - 1)):
+#     #         smallerWordsRemoved += ' ' + word
+#     return allWords
