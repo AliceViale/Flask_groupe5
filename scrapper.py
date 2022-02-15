0d66f49026fc9e86fdea17f988a3f48be6ee2d65
Jean Charbonneau <jcharbonneau.ext@simplonformations.onmicrosoft.com>
2022-02-15 09:46:27 +0100

    Small modification

--- scrapper.py
+++ scrapper.py
@@ -3,7 +3,7 @@ import re
 from bs4 import BeautifulSoup
 
 def getTextFromUrl(url : str, nbChars : int):
-    """Scraps text from a given url and return a string of text only words of nbChars length."""
+    """Scraps text from a given url and returns a string of text only words of nbChars length."""
     res = requests.get(url)
     html_page = res.content
     soup = BeautifulSoup(html_page, 'html.parser')
@@ -13,7 +13,7 @@ def getTextFromUrl(url : str, nbChars : int):
     for strPiece in soup.stripped_strings:
         allWords += ' ' + strPiece.lower()
 
-    allWords = re.sub("[^a-zA-Zàâäéèêëïîôöùûüÿç]+", " ", allWords)
+    allWords = re.sub("[^a-zàâäéèêëïîôöùûüÿç]+", " ", allWords)
 
     smallerWordsRemoved = ''
 
@@ -23,6 +23,6 @@ def getTextFromUrl(url : str, nbChars : int):
 
     return smallerWordsRemoved
 
-# print(getTextFromUrl('https://fr.wikipedia.org/wiki/R%C3%A9cepteur_sensoriel'))
+# print(getTextFromUrl('https://fr.wikipedia.org/wiki/R%C3%A9cepteur_sensoriel', 6))
 
 
